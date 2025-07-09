c86a8172c51c1890552193bf96f88b0cecf455c1
eli-wint <e.winton.cc@gmail.com>
2025-07-09 00:43:41 -0700

    Gutted code

--- src/main/java/frc/robot/subsystems/FlipperSubsystem.java
+++ src/main/java/frc/robot/subsystems/FlipperSubsystem.java
@@ -9,47 +9,23 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class FlipperSubsystem  extends SubsystemBase {
-    private final SparkMax flipperMotor;
-    double speed = 0;
 
     public FlipperSubsystem() {
-        flipperMotor = new SparkMax(Constants.IDs.FLIPPER_MOTOR, MotorType.kBrushless);
-        SparkMaxConfig flipperMotorConfig = new SparkMaxConfig();
-        flipperMotorConfig
-        .inverted(true)
-        .idleMode(IdleMode.kBrake)
-        .smartCurrentLimit(10);
-        flipperMotor.configure(flipperMotorConfig, null, null);
     }
 
     public void setSpeed(double speed) {
-        this.speed = speed;
     }
 
     public void resetPivotEncoder() {
-        flipperMotor.getEncoder().setPosition(0);
     }
 
     public void stop() {
-        flipperMotor.set(0);
-
     }
 
-    public double getFlipperPosition() {
-        return flipperMotor.getEncoder().getPosition();
+    public void getFlipperPosition() { // double
     }
 
     @Override
     public void periodic() {
-
-        if ((getFlipperPosition() <= Constants.LOWER_ENDPOINT &&
-                speed < 0) || (getFlipperPosition() >= Constants.UPPER_ENDPOINT && speed > 0)) {
-            System.out.println("Flipper ENDPOINT reached!!!");
-            speed = 0;
-        }
-
-        SmartDashboard.putNumber("Flipper Encoder", getFlipperPosition());
-
-        flipperMotor.set(speed);
     }
 }
