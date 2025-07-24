4490dec0e26ed81e8b07dbae36e338869980d2fb
eli-wint <e.winton.cc@gmail.com>
2025-07-23 18:22:42 -0700

    more testing

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025MiniBotRewrite";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 9;
-  public static final String GIT_SHA = "1d539296b64da242a1d7080bc89e7ab4b3028d7c";
-  public static final String GIT_DATE = "2025-07-23 03:02:04 EDT";
+  public static final int GIT_REVISION = 10;
+  public static final String GIT_SHA = "50376c6b85ce37e3022d8b6f4af240528b87f3bb";
+  public static final String GIT_DATE = "2025-07-23 17:26:27 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-07-23 17:06:42 EDT";
-  public static final long BUILD_UNIX_TIME = 1753304802435L;
+  public static final String BUILD_DATE = "2025-07-23 21:21:09 EDT";
+  public static final long BUILD_UNIX_TIME = 1753320069643L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
