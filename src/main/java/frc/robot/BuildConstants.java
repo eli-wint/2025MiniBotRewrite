78f6728d79d49072411018ecedb502907af684c1
eli-wint <e.winton.cc@gmail.com>
2025-09-02 21:00:51 -0700

    saved a file

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025MiniBotRewrite";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 15;
-  public static final String GIT_SHA = "f009a6b876200c982aa352c0c24f0a41fd46386a";
-  public static final String GIT_DATE = "2025-08-14 16:50:14 EDT";
+  public static final int GIT_REVISION = 16;
+  public static final String GIT_SHA = "28d1d43aad7e0e91f162eaa13d762e38c6f95bfb";
+  public static final String GIT_DATE = "2025-08-16 22:15:22 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-08-16 22:13:14 EDT";
-  public static final long BUILD_UNIX_TIME = 1755396794795L;
+  public static final String BUILD_DATE = "2025-09-02 23:58:21 EDT";
+  public static final long BUILD_UNIX_TIME = 1756871901052L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/subsystems/flipper/FlipperConstants.java
+++ src/main/java/frc/robot/subsystems/flipper/FlipperConstants.java
@@ -12,7 +12,7 @@ public final class FlipperConstants {
   // Flipper Constants
   public static final double UPPER_ENDPOINT = 1.0; // -7
   public static final double LOWER_ENDPOINT = -8.0; // -17
-  public static final double FLIPPER_PARKED = 1;
+  public static final double FLIPPER_PARKED = 1; 
   public static final double FLIPPER_TO_CLOSE = -10;
   public static final double FLIPPER_DOWN = -16.5;
   public static final double TOLERANCE = 0.5;
